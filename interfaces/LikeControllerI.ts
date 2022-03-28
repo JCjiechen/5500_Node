82d203c4bff3efe3512c00e49ccdb045606cb286
JC <jc.jiechen.jane@gmail.com>
2022-03-28 18:02:36 -0400

    dislikes interface

--- interfaces/LikeControllerI.ts
+++ interfaces/LikeControllerI.ts
@@ -1,7 +1,14 @@
-import {Request, Response} from "express";
+import { Request, Response } from "express";
 
+/**
+ * @file Declares likes and dislikes controller interface.
+ */
 export default interface LikeControllerI {
-    findAllUsersThatLikedTuit (req: Request, res: Response): void;
-    findAllTuitsLikedByUser (req: Request, res: Response): void;
-    userTogglesTuitLikes (req: Request, res: Response): void;
+    findAllUsersThatLikedTuit(req: Request, res: Response): void;
+    findAllTuitsLikedByUser(req: Request, res: Response): void;
+    userTogglesTuitLikes(req: Request, res: Response): void;
+
+    findAllUsersThatDislikedTuit(req: Request, res: Response): void;
+    findAllTuitsDislikedByUser(req: Request, res: Response): void;
+    userTogglesTuitDislikes(req: Request, res: Response): void;
 };
