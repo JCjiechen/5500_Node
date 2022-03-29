063d6c8c3c76679359f88d2d049f65883579d16f
JC <jc.jiechen.jane@gmail.com>
2022-03-28 23:22:21 -0400

    dislike interface

--- interfaces/LikeControllerI.ts
+++ interfaces/LikeControllerI.ts
@@ -1,14 +1,10 @@
 import { Request, Response } from "express";
 
 /**
- * @file Declares likes and dislikes controller interface.
+ * @file Declares likes controller interface.
  */
 export default interface LikeControllerI {
     findAllUsersThatLikedTuit(req: Request, res: Response): void;
     findAllTuitsLikedByUser(req: Request, res: Response): void;
     userTogglesTuitLikes(req: Request, res: Response): void;
-
-    findAllUsersThatDislikedTuit(req: Request, res: Response): void;
-    findAllTuitsDislikedByUser(req: Request, res: Response): void;
-    userTogglesTuitDislikes(req: Request, res: Response): void;
 };
--- interfaces/LikeDaoI.ts
+++ interfaces/LikeDaoI.ts
@@ -1,17 +1,11 @@
 import Like from "../models/likes/Like";
-import Dislike from "../models/likes/Dislike";
 
 /**
- * @file Declares API for Likes and Dislikes related data access object methods
+ * @file Declares API for Likes related data access object methods
  */
 export default interface LikeDaoI {
     findAllUsersThatLikedTuit(tid: string): Promise<Like[]>;
     findAllTuitsLikedByUser(uid: string): Promise<Like[]>;
     userUnlikesTuit(tid: string, uid: string): Promise<any>;
     userLikesTuit(tid: string, uid: string): Promise<Like>;
-
-    findAllUsersThatDislikedTuit(tid: string): Promise<Dislike[]>;
-    findAllTuitsDislikedByUser(uid: string): Promise<Dislike[]>;
-    userUndislikesTuit(tid: string, uid: string): Promise<any>;
-    userDislikesTuit(tid: string, uid: string): Promise<Dislike>;
 };
