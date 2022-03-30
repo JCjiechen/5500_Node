4034e8351df6e44f374daad23dc80644e4fe16b4
JC <jc.jiechen.jane@gmail.com>
2022-03-29 20:14:08 -0400

    fix bugs

--- mongoose/likes/DislikeSchema.ts
+++ mongoose/likes/DislikeSchema.ts
@@ -4,5 +4,5 @@ import Dislike from "../../models/likes/Dislike";
 const DislikeSchema = new mongoose.Schema<Dislike>({
     tuit: { type: Schema.Types.ObjectId, ref: "TuitModel" },
     dislikedBy: { type: Schema.Types.ObjectId, ref: "UserModel" },
-}, { collection: "likes" });
+}, { collection: "dislikes" });
 export default DislikeSchema;
