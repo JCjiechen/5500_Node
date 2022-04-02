16242b7cabd289a0fae1e4e1cb08fd1889560cb5
JC <jc.jiechen.jane@gmail.com>
2022-04-01 21:46:22 -0400

    add DislikSchema comments

--- mongoose/likes/DislikeSchema.ts
+++ mongoose/likes/DislikeSchema.ts
@@ -1,6 +1,14 @@
+/**
+ * @file Implements mongoose schema for dislikes
+ */
 import mongoose, { Schema } from "mongoose";
 import Dislike from "../../models/likes/Dislike";
 
+/**
+ * @typedef Dislike Represents dislikes
+ * @property {ObjectId[]} tuit The tuits
+ * @property {ObjectId[]} dislikeedBy dislikes user
+ */
 const DislikeSchema = new mongoose.Schema<Dislike>({
     tuit: { type: Schema.Types.ObjectId, ref: "TuitModel" },
     dislikedBy: { type: Schema.Types.ObjectId, ref: "UserModel" },
